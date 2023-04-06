01e5a8b0c3b4ea6af655797b68e218159177e416
Breno Souza Oliveira <soubreno@gmail.com>
2023-04-06 23:58:41 +0100

    Update configurar.sh

--- configuracoes/fedora/configurar.sh
+++ configuracoes/fedora/configurar.sh
@@ -35,62 +35,62 @@ read inputApps
 
 if [[ $inputApps == "s" ]];
 then
-    echo "-----------" >> $1
-    echo "Apps" >> $1
-    echo "-----------" >> $1
+    echo "-----------" >> $log
+    echo "Apps" >> $log
+    echo "-----------" >> $log
 
-    echo "Instalando wget, curl e gpg.." >> $1
+    echo "Instalando wget, curl e gpg.." >> $log
 
     if dnf install wget curl gpg;
     then
-        echo "Wget, curl e gpg instalados.." >> $1
+        echo "Wget, curl e gpg instalados.." >> $log
     else
-        echo "Erro ao instalar wget, curl e gpg.." >> $1
+        echo "Erro ao instalar wget, curl e gpg.." >> $log
         exit 1
     fi
 
-    echo >> $1
-    echo "------" >> $1
-    echo >> $1
+    echo >> $log
+    echo "------" >> $log
+    echo >> $log
 
-    echo "Instalando zsh.." >> $1
+    echo "Instalando zsh.." >> $log
 
     if dnf install zsh \
     && usermod -s "$(which zsh)" $USER;
     then
-        echo "Zsh instalado.." >> $1
+        echo "Zsh instalado.." >> $log
     else
-        echo "Erro ao instalar zsh.." >> $1
+        echo "Erro ao instalar zsh.." >> $log
     fi
 
-    echo >> $1
-    echo "------" >> $1
-    echo >> $1
+    echo >> $log
+    echo "------" >> $log
+    echo >> $log
 
-    echo "Instalando Discord.." >> $1
+    echo "Instalando Discord.." >> $log
 
     if dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
     && dnf update \
     && dnf install discord;
     then
-        echo "Discord instalado.." >> $1
+        echo "Discord instalado.." >> $log
     else
-        echo "Erro ao instalar Discord.." >> $1
+        echo "Erro ao instalar Discord.." >> $log
     fi
 
-    echo >> $1
-    echo "------" >> $1
-    echo >> $1
+    echo >> $log
+    echo "------" >> $log
+    echo >> $log
 
-    echo "Instalando Brave.." >> $1
+    echo "Instalando Brave.." >> $log
 
     if dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ \
     && rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc \
     && dnf install brave-browser -y;
     then
-        echo "Bravo instalado.." >> $1
+        echo "Bravo instalado.." >> $log
     else
-        echo "Erro ao instalar Brave.." >> $1
+        echo "Erro ao instalar Brave.." >> $log
     fi
 fi
 
@@ -101,33 +101,33 @@ read input
 
 if [[ $input == "s" ]];
 then
-        echo "-----------" >> $1
-    echo "Apps Dev" >> $1
-    echo "-----------" >> $1
+        echo "-----------" >> $log
+    echo "Apps Dev" >> $log
+    echo "-----------" >> $log
 
-    echo "Instalando VS Code.." >> $1
+    echo "Instalando VS Code.." >> $log
 
     if rpm --import https://packages.microsoft.com/keys/microsoft.asc \
     && sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo' \
     && dnf check-update \
     && dnf install code;
     then
-        echo "VS Code instalado.." >> $1
+        echo "VS Code instalado.." >> $log
     else
-        echo "Erro ao instalar VS Code.." >> $1
+        echo "Erro ao instalar VS Code.." >> $log
         exit 1
     fi
 
-    echo >> $1
-    echo "------" >> $1
-    echo >> $1
+    echo >> $log
+    echo "------" >> $log
+    echo >> $log
 
     echo "Path do arquivo GO baixado? (Deixar em branco para NAO instalar)"
     read pathGo
 
     if [[ $pathGo != "" ]];
     then
-        echo "Instalando GO.." >> $1
+        echo "Instalando GO.." >> $log
         if [ -d "/usr/local/go" ]
         then
             rm -rf /usr/local/go;
@@ -137,15 +137,15 @@ then
         then
             export PATH=$PATH:/usr/local/go/bin
             go version
-            echo "GO instalado.." >> $1
+            echo "GO instalado.." >> $log
         else
-            echo "Erro ao instalar GO.." >> $1
+            echo "Erro ao instalar GO.." >> $log
             exit 1
         fi
 
-        echo >> $1
-        echo "------" >> $1
-        echo >> $1
+        echo >> $log
+        echo "------" >> $log
+        echo >> $log
     fi
 
 
@@ -154,24 +154,24 @@ then
 
     if [[ $inputDbEaver == "s" ]];
     then
-        echo "Instalando DBeaver.." >> $1
+        echo "Instalando DBeaver.." >> $log
 
         if dnf java-17-openjdk-devel \
         && yum -y install wget \
         && rpm -Uvh ./dbeaver-ce-latest-stable.x86_64.rpm;
         then
-            echo "DBeaver instalado.." >> $1
+            echo "DBeaver instalado.." >> $log
         else
-            echo "Erro ao instalar DBeaver.." >> $1
+            echo "Erro ao instalar DBeaver.." >> $log
             exit 1
         fi
 
-        echo >> $1
-        echo "------" >> $1
-        echo >> $1
+        echo >> $log
+        echo "------" >> $log
+        echo >> $log
     fi
 
-    echo "Instalando Docker.." >> $1
+    echo "Instalando Docker.." >> $log
 
     if dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine; \
     dnf -y install dnf-plugins-core \
@@ -179,66 +179,66 @@ then
     && dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin \
     && systemctl start docker;
     then
-        echo "Docker instalado.." >> $1
+        echo "Docker instalado.." >> $log
     else
-        echo "Erro ao instalar Docker.." >> $1
+        echo "Erro ao instalar Docker.." >> $log
         exit 1
     fi
 
-    echo >> $1
-    echo "------" >> $1
-    echo >> $1
+    echo >> $log
+    echo "------" >> $log
+    echo >> $log
 
-    echo "Instalando Postgre no docker.." >> $1
+    echo "Instalando Postgre no docker.." >> $log
 
     if docker run --name postgres -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres postgres;
     then
-        echo "Postgre instalado no docker.." >> $1
+        echo "Postgre instalado no docker.." >> $log
     else
-        echo "Erro ao instalar postgre no docker.." >> $1
+        echo "Erro ao instalar postgre no docker.." >> $log
     fi
 
-    echo >> $1
-    echo "------" >> $1
-    echo >> $1
+    echo >> $log
+    echo "------" >> $log
+    echo >> $log
 
-    echo "Instalando MySQL no docker.." >> $1
-    echo "---------------------" >> $1
-    echo >> $1
+    echo "Instalando MySQL no docker.." >> $log
+    echo "---------------------" >> $log
+    echo >> $log
 
     if docker run --name mysql -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=12345678 mysql;
     then
         echo
-        echo "Postgre instalado no docker.." >> $1
+        echo "Postgre instalado no docker.." >> $log
     else
-        echo "Erro ao instalar postgre no docker.." >> $1
+        echo "Erro ao instalar postgre no docker.." >> $log
     fi
 
-    echo >> $1
-    echo "------" >> $1
-    echo >> $1
+    echo >> $log
+    echo "------" >> $log
+    echo >> $log
 
     echo "Instalar MySQL Workbench? [s,N]"
     read inputMSQLWb
 
     if [[ $inputMSQLWb == "s" ]];
     then
-        echo "Instalando MySQL Workbench.." >> $1
-        echo "---------------------" >> $1
-        echo >> $1
+        echo "Instalando MySQL Workbench.." >> $log
+        echo "---------------------" >> $log
+        echo >> $log
 
         if rpm -Uvh mysql80-community-release-* \
         && yum install mysql-workbench;
         then
             echo
-            echo "MySQLWorkbench instalado.." >> $1
+            echo "MySQLWorkbench instalado.." >> $log
         else
-            echo "Erro ao instalar MySQLWorkbench.." >> $1
+            echo "Erro ao instalar MySQLWorkbench.." >> $log
         fi
 
-        echo >> $1
-        echo "------" >> $1
-        echo >> $1
+        echo >> $log
+        echo "------" >> $log
+        echo >> $log
     fi
 fi
 
