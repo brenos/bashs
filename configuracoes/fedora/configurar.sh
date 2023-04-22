10a949804d77c61c4ff1eb0782ec7d1bf1161348
Breno Souza Oliveira <soubreno@gmail.com>
2023-04-22 11:47:36 +0100

    Remove brave

--- configuracoes/fedora/configurar.sh
+++ configuracoes/fedora/configurar.sh
@@ -81,17 +81,6 @@ then
     echo >> $log
     echo "------" >> $log
     echo >> $log
-
-    echo "Instalando Brave.." >> $log
-
-    if dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ \
-    && rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc \
-    && dnf install brave-browser -y;
-    then
-        echo "Bravo instalado.." >> $log
-    else
-        echo "Erro ao instalar Brave.." >> $log
-    fi
 fi
 
 echo "-----------------------------" >> $log
